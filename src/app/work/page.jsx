26bd29237c2b179d08e15103ffea4f76564f3c29
mhardik003 <mhardik2003@gmail.com>
2025-01-24 18:45:25 +0530

    Added publications to the webiste

--- src/app/work/page.jsx
+++ src/app/work/page.jsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import { Button } from '@/components/Button' // Add Button component here
 import { SimpleLayout1 } from '@/components/SimpleLayout'
 import CVIT from '@/images/logos/cvit.png'
+import SEMEVAL from '@/images/logos/semeval.jpeg'
 import AMS from '@/images/logos/ams.jpeg'
 import ECELL from '@/images/logos/ecell.png'
 import IIITH from '@/images/logos/iiith.png'
@@ -194,6 +195,71 @@ function Resume() {
   )
 }
 
+function Publications() {
+  let publications = [
+    {
+      title: `Exploring Multimodal Emotion Cause Pair Extraction as Sequence Labelling Task`,
+      conf: 'Semeval',
+      logo: SEMEVAL,
+      publication_date: '2024',
+      link:'https://arxiv.org/pdf/2404.02088'
+    },    
+    {
+      title: `On the trail of textual origins: Roberta-bilstm approach to detect ai-generated text`,
+      conf: 'Semeval',
+      logo: SEMEVAL,
+      publication_date: '2024',
+      link:'https://arxiv.org/pdf/2407.02978'
+    },
+    
+  ]
+
+  return (
+    <div className="mt-8 rounded-2xl border border-zinc-100 p-6 dark:border-zinc-700/40">
+      <h2 className="flex text-sm font-semibold text-zinc-900 dark:text-zinc-100">
+        <CertificationsIcon className="h-6 w-6 flex-none" />
+        <span className="ml-3">Publications</span>
+      </h2>
+      <ol className="mt-6 space-y-4">
+        {publications.map((publication, index) => (
+          <Publication key={index} publication={publication} />
+        ))}
+      </ol>
+    </div>
+  )
+} 
+
+function Publication({ publication }) {
+  let startLabel =
+    typeof publication.publication_date === 'string'
+      ? publication.publication_date
+      : publication.publication_date.label;
+
+  return (
+    <a href={publication.link} target="_blank" rel="noopener noreferrer">
+      <li className="flex gap-4 hover:bg-zinc-100 dark:hover:bg-zinc-800 p-2 rounded-lg transition-colors">
+        <div className="relative mt-1 flex h-10 w-10 flex-none items-center justify-center rounded-full shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
+          <Image src={publication.logo} alt="" className="h-7 w-7" unoptimized />
+        </div>
+        <dl className="flex flex-auto flex-wrap gap-x-2">
+          <dt className="sr-only">Conference</dt>
+          <dd className="w-full flex-none text-sm font-medium text-zinc-900 dark:text-zinc-100">
+            {publication.conf}
+          </dd>
+          <dt className="sr-only">Paper title</dt>
+          <dd className="text-xs text-zinc-500 dark:text-zinc-400">
+            {publication.title}
+          </dd>
+          <dt className="sr-only">Publication Date</dt>
+          <dd className="ml-auto text-xs text-zinc-400 dark:text-zinc-500">
+            <time dateTime={publication.publication_date}>{startLabel}</time>
+          </dd>
+        </dl>
+      </li>
+    </a>
+  );
+}
+
 function Achievements() {
   let achievements = [
     {
@@ -351,6 +417,7 @@ export default function Speaking() {
             <ArrowDownIcon className="h-4 w-4 stroke-zinc-400 transition group-active:stroke-zinc-600 dark:group-hover:stroke-zinc-50 dark:group-active:stroke-zinc-50" />
         </Button>
     <Resume />
+    <Publications />
     <Achievements />
     <Educations />
 
