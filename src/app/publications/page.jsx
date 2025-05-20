6d188ad27545ad697c4c60c30d4f256fb037ab53
mhardik003 <mhardik2003@gmail.com>
2025-05-20 12:02:45 +0530

    Added talks section

--- src/app/publications/page.jsx
+++ src/app/publications/page.jsx
@@ -66,42 +66,74 @@ function ArrowDownIcon(props) {
   )
 }
 
-function Role({ role }) {
-  let startLabel =
-    typeof role.start === 'string' ? role.start : role.start.label
-  let startDate =
-    typeof role.start === 'string' ? role.start : role.start.dateTime
 
-  let endLabel = typeof role.end === 'string' ? role.end : role.end.label
-  let endDate = typeof role.end === 'string' ? role.end : role.end.dateTime
+function Talks() {
+  let talks = [
+    {
+      title: `WandB & Ada`,
+      conf: 'CVIT, IIITH',
+      logo: CVIT,
+      talk_date: '2024',
+      link:'https://docs.google.com/presentation/d/1ycP_7qe255JJ7ODp3iPiWvlkPVwGgYn1SWvGOcI_KKY/edit?slide=id.p#slide=id.p'
+    },
+    {
+      title: `Entrepreneurship 101`,
+      conf: 'E-Cell, IIITH',
+      logo: ECELL,
+      talk_date: '2024',
+      link:'https://docs.google.com/presentation/d/1z6S5TRDcnX5YKobPb5an35_mmtXzDRRxFT9QaMEtIXw/edit?usp=sharing'
+    },    
+    
+  ]
 
   return (
-    <li className="flex gap-4">
-      <div className="relative mt-1 flex h-10 w-10 flex-none items-center justify-center rounded-full shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
-        <Image src={role.logo} alt="" className="h-7 w-7" unoptimized />
-      </div>
-      <dl className="flex flex-auto flex-wrap gap-x-2">
-        <dt className="sr-only">Company</dt>
-        <dd className="w-full flex-none text-sm font-medium text-zinc-900 dark:text-zinc-100">
-          {role.company}
-        </dd>
-        <dt className="sr-only">Role</dt>
-        <dd className="text-xs text-zinc-500 dark:text-zinc-400">
-          {role.title}
-        </dd>
-        <dt className="sr-only">Date</dt>
-        <dd
-          className="ml-auto text-xs text-zinc-400 dark:text-zinc-500"
-          aria-label={`${startLabel} until ${endLabel}`}
-        >
-          <time dateTime={startDate}>{startLabel}</time>{' '}
-          <span aria-hidden="true">—</span>{' '}
-          <time dateTime={endDate}>{endLabel}</time>
-        </dd>
-      </dl>
-    </li>
+    <div className="mt-8 rounded-2xl border border-zinc-100 p-6 dark:border-zinc-700/40">
+      <h2 className="flex text-sm font-semibold text-zinc-900 dark:text-zinc-100">
+        <CertificationsIcon className="h-6 w-6 flex-none" />
+        <span className="ml-3">Talks</span>
+      </h2>
+      <ol className="mt-6 space-y-4">
+        {talks.map((talk, index) => (
+          <Talk key={index} talk={talk} />
+        ))}
+      </ol>
+    </div>
   )
+} 
+
+function Talk({ talk }) {
+  let startLabel =
+    typeof talk.talk_date === 'string'
+      ? talk.talk_date
+      : talk.talk_date.label;
+
+  return (
+    <a href={talk.link} target="_blank" rel="noopener noreferrer">
+      <li className="flex gap-4 hover:bg-zinc-100 dark:hover:bg-zinc-800 p-2 rounded-lg transition-colors">
+        <div className="relative mt-1 flex h-10 w-10 flex-none items-center justify-center rounded-full shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
+          <Image src={talk.logo} alt="" className="h-7 w-7" unoptimized />
+        </div>
+        <dl className="flex flex-auto flex-wrap gap-x-2">
+          <dt className="sr-only">Event</dt>
+          <dd className="w-full flex-none text-sm font-medium text-zinc-900 dark:text-zinc-100">
+            {talk.conf}
+          </dd>
+          <dt className="sr-only">Topic</dt>
+          <dd className="text-xs text-zinc-500 dark:text-zinc-400">
+            {talk.title}
+          </dd>
+          <dt className="sr-only">Talk Date</dt>
+          <dd className="ml-auto text-xs text-zinc-400 dark:text-zinc-500">
+            <time dateTime={talk.talk_date}>{startLabel}</time>
+          </dd>
+        </dl>
+      </li>
+    </a>
+  );
 }
+
+
+
 
 function Publications() {
   let publications = [
@@ -179,6 +211,7 @@ export default function Speaking() {
     >
 
     <Publications />
+    <Talks />
 
     </SimpleLayout1>
   )
