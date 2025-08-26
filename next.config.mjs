70b96fe4280d984715fe0632472112a1a75ddb3c
mhardik003 <mhardik2003@gmail.com>
2025-08-26 16:29:13 +0530

    external social links

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,26 @@ const nextConfig = {
       '/articles/*': ['./src/app/articles/**/*.mdx'],
     },
   },
+
+  async redirects() {
+    return [
+      {
+        source: '/github',
+        destination: 'https://github.com/mhardik003', // replace with your GitHub
+        permanent: true,
+      },
+      {
+        source: '/linkedin',
+        destination: 'https://www.linkedin.com/in/mhardik003', // replace with LinkedIn
+        permanent: true,
+      },
+      {
+        source: '/resume',
+        destination: 'https://drive.google.com/file/d/1m3oXrEpFwdDaTj1w0uONXwGs5C-9dFaw/view?usp=sharing', // or a Google Drive/Notion link
+        permanent: true,
+      },
+    ]
+  },
 }
 
 const withMDX = nextMDX({
